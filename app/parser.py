5af9af59201e5f719d93cfca027d9ba44e9923ce
Meghan Maloy <meghan.maloy@datadoghq.com>
2020-04-28 19:19:33 -0400

    adding regex special character replacement

--- app/parser.py
+++ app/parser.py
@@ -1,10 +1,13 @@
 from scraper import scrape;
 import inflect;
+import re;
 
 def parse(input_word):
     plural_engine = inflect.engine()
 
     parse_string = scrape().lower();
+
+    parse_string = re.sub("[^A-Za-z0-9]+", " ", parse_string)
 
     singular_count = parse_string.count(" " + input_word + " ")
 
@@ -12,4 +15,5 @@ def parse(input_word):
     plural_count = parse_string.count(" " + plural_word + " ")
 
     total_count = singular_count + plural_count
+    print(total_count)
     return total_count;
