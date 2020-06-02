87074b565f52328d6b156289c5b239ba97525d0e
djchinia <dan.chiniara@datadoghq.com>
2020-06-02 19:52:49 -0400

    frontend-form

--- fe-api/frontend/src/App.js
+++ fe-api/frontend/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import Graph from './components/Graph'
+import Form from './components/Form'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex-row">
+        <div className = "padding">
+        <Form/>
+        </div>
+        <div className = "padding">
+        <Graph/>
+        </div>
+      </div>
     </div>
   );
 }
