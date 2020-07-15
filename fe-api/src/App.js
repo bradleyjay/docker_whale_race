46388433414906cda885efef2d176a1c100079f8
bradleyjay <bradleyjshields@gmail.com>
2020-07-14 22:04:06 -0400

    Added basic React websocket implimentation, express server.

--- fe-api/src/App.js
+++ fe-api/src/App.js
@@ -1,33 +1,33 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Main from './components/Main'
 import './App.css';
 import socketIOClient from 'socket.io-client';
 
 // https://dev.to/captainpandaz/a-socket-io-tutorial-that-isn-t-a-chat-app-with-react-js-58jh
-
-class App extends Component{
+// This socket.io is needed to pass data from express server to react itself
+class App extends Component {
   constructor() {
     super();
     this.state = {
-        response: 0,
-        endpoint: "http://127.0.0.1:4001"
+      response: 0,
+      endpoint: "http://127.0.0.1:4001"
     };
   }
   componentDidMount() {
-    const {endpoint} = this.state;
+    const { endpoint } = this.state;
     // connect to the socket
     const socket = socketIOClient(endpoint);
     // listen for outgoing data and supply a callback
     // updates the state var when receiving a callback
-    socket.on("outgoing data", data => this.setState({response: data.num}));
+    socket.on("outgoing data", data => this.setState({ response: data.num }));
   }
 
   render() {
-    const {response} = this.state
-    return(
+    const { response } = this.state
+    return (
       <div className="App">
         <Main
-          response={this.response}     
+        // response={this.response}
         />
       </div>
     )
