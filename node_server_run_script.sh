a9d6a08b4d688f595127611140f1c6e28ca27787
Meghan Maloy <meghan.maloy@datadoghq.com>
2020-11-04 19:54:19 -0500

    working runscript

---
+++ node_server_run_script.sh
@@ -0,0 +1,4 @@
+echo "HEY MADE IT TO SERVER"
+cd /Users/meghan.maloy/dev/docker_whale_race #REPLACE THIS LINE
+cd fe-api/server
+node server.js
--- run_script.sh
+++ run_script.sh
@@ -1,7 +1,7 @@
 # Check that docker is up (section 0)
 result=$( docker ps )
 
-if [[ -n "$result" ]]; 
+if [[ -n "$result" ]];
 then
   echo $result
   echo "Docker is up"
@@ -12,18 +12,22 @@ else
   sleep 25 # wait 25s (maybe we can shorteen it)
 fi
 
-#  Setup the Frontend (Step 4)
-cd fe-api
-brew install node
-npm install
-npm start &  #FIXME this shuts immediately.
-# nohup npm start &
+# Update other run scripts to use the right directory
+pwdesc=$(echo $PWD | sed 's_/_\\/_g')
 
-echo "HEY MADE IT TO SERVER"
-cd server
-node server.js &  #FIXME this may not run, needs own terminal?
-# nohup node server.js &
-cd ../..
+sed -i '' "/#REPLACE THIS LINE/s/.*/cd $pwdesc #REPLACE THIS LINE/g" react_run_script.sh
+sed -i '' "/#REPLACE THIS LINE/s/.*/cd $pwdesc #REPLACE THIS LINE/g" node_server_run_script.sh
+
+chmod +x react_run_script.sh
+chmod +x node_server_run_script.sh
+
+sleep 2
+
+# RUN REACT SCRIPT
+open -a Terminal react_run_script.sh
+
+# RUN NODE SERVER SCRIPT
+open -a Terminal node_server_run_script.sh
 
 # Initialization Step in readme.md (section 1)
 python3 -m venv venv
@@ -35,10 +39,10 @@ then
   echo "enthusiastically in virtual environment"
   pip3 install -r docker/requirements.txt
   # Docker Preparation (section 3)
-  docker build --no-cache -t docker_shocker ./docker 
+  docker build --no-cache -t docker_shocker ./docker
   # Start the API Flask Server (section 2)
   chmod +x docker/run_api.sh
-  echo "changed permissions"  
+  echo "changed permissions"
   # Open up a new Terminal for run_api.sh
   # open -a Terminal ./docker/run_api.sh &
   cd docker
@@ -48,4 +52,3 @@ else
   # INVENV=0
   echo "not in virtual environment"
 fi
-
